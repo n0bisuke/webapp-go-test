60602f745996c1e9fcad582d3dc6270df4bca053
のびすけ <rsksound@gmail.com>
2016-12-03 13:00:41 +0900

    port調整

--- app.go
+++ app.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+    "os"
+    "fmt"
+    "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, World!!!!")
+    fmt.Fprintf(w, "Hello, World!!!!")
 }
 
+
+
 func main() {
-  http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-  http.ListenAndServe(":8080", nil)
+    PORT := ":"
+    PORT += "3000"
+    if os.Getenv("PORT") != "" {
+        PORT += os.Getenv("PORT")
+    }
+    fmt.Println("port is", PORT)
+    
+    http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+    http.ListenAndServe(PORT, nil)
 }
