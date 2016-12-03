dc383b91774786421331aebbd04e1509199f9656
のびすけ <rsksound@gmail.com>
2016-12-03 18:33:22 +0900

    edit server.go

--- server.go
+++ server.go
@@ -1,25 +1,39 @@
-package main
+// package main
 
-import (
-    "os"
+// import (
+//     "os"
+//     "fmt"
+//     "net/http"
+// )
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintf(w, "Hello, World!!!!")
+// }
+
+
+
+// func main() {
+//     PORT := ":"
+//     PORT += "3000"
+//     if os.Getenv("PORT") != "" {
+//         PORT += os.Getenv("PORT")
+//     }
+//     fmt.Println("port is", PORT)
+    
+//     http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+//     http.ListenAndServe(PORT, nil)
+// }
+
+package main  
+import (  
     "fmt"
     "net/http"
+    "os" 
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!!!!")
+func handler(w http.ResponseWriter, r *http.Request) {  
+    fmt.Fprintf(w, "Hi there - this page was served using Go \\o/")
 }
-
-
-
-func main() {
-    PORT := ":"
-    PORT += "3000"
-    if os.Getenv("PORT") != "" {
-        PORT += os.Getenv("PORT")
-    }
-    fmt.Println("port is", PORT)
-    
-    http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-    http.ListenAndServe(PORT, nil)
+func main() {  
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":"+os.Getenv("HTTP_PLATFORM_PORT"), nil)
 }
